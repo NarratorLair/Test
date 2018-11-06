74a5686f4b44d925b9b48a653a5b7429ebb43796
NarratorLair <jakubparkita88@gmail.com>
2018-11-06 11:10:21 +0100

    v0.2 Included IP adress check-on.

--- skrypt.bat
+++ skrypt.bat
@@ -1,7 +1,12 @@
-echo Sprawdzanie katalogu C://
+echo Sprawdzanie katalogu C:// > historia.txt
 cd /
-tree
-echo Tworzenie użytkownika testowego.
-net user create Test zaq1@WSX
-echo Utworzono.
+tree >> historia.txt
+echo Tworzenie użytkownika testowego. >> historia.txt
+net user create Test zaq1@WSX >> historia.txt
+echo Utworzono konto. Sprawdzanie adresu IPv4 i IPv6. >> historia.txt
+ipconfig -A >> historia.txt
+echo Sprawdzenie przesyłu na trasie Ameryka-obecny host. >> historia.txt
+ping www.microsoft.com >> historia.txt
+tracert www.microsoft.com >> historia.txt
+echo Skrypt wykonany.
 pause 12
